7b252df132c634034da2efb0c6e3774c50f9bdd3
Yashraj Chouhan <129044960+Jadu07@users.noreply.github.com>
2025-04-08 00:10:10 +0530

    Time: 58 ms (8.06%) | Memory: 20 MB (18.59%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -10,7 +10,7 @@ class Solution:
             return False
         slow=head
         fast=head
-        while fast and fast.next:
+        while fast and fast.next!=None:
             slow=slow.next
             fast=fast.next.next
             if slow==fast:
