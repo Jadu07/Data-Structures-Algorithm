4478ba24791b7d4ea844f7b585b02b66a4edbee8
Yashraj Chouhan <129044960+Jadu07@users.noreply.github.com>
2025-05-03 15:25:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (66.92%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -3,16 +3,11 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        #Transpose
-        #Iterate for i in range(len(matrix))
+       
         for i in range(len(matrix)):
-            #Iterate for j in range(i, len(matrix))
             for j in range(i, len(matrix)):
-                #Switch values in matrix[i][j] with matrix[j][i]
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-        #Reverse
-        #Iterate for row in matrix
+      
         for row in matrix:
-            #reverse each row to get vertical mirror image of the transposed matrix
             row.reverse()
