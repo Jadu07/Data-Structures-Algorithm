4ed69b7ca9f03fbcfe5c1ecced832797aaf93dcb
Yashraj Chouhan <129044960+Jadu07@users.noreply.github.com>
2025-08-28 10:05:12 +0530

    Time: 10 ms (88.15%) | Memory: 23.8 MB (43.80%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -11,8 +11,9 @@ class KthLargest:
     def add(self, val: int) -> int:
         if len(self.heap)<self.k:
             heapq.heappush(self.heap,val)
-        elif val>self.heap[0]:
-            heapq.heapreplace(self.heap,val)
+        elif val > self.heap[0]:
+            heapq.heappop(self.heap)       
+            heapq.heappush(self.heap, val) 
         return self.heap[0]
         
 
